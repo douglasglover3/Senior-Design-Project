90a0bc613b9e11073528888d6ae2b410cae51af1
Hope4Worthy <brantdeines01@gmail.com>
2022-10-08 13:17:10 -0400

    Cleanup unsued elements and style color box

--- src/windows/MainWindow.js
+++ src/windows/MainWindow.js
@@ -3,6 +3,7 @@ import Select from "react-select";
 const fs = window.require('fs');
 const path = window.require('path');
 
+// set of colors for testing purposes, can be removed later
 const options =
   [
     { value: '#000000', label: "Black" },
@@ -12,12 +13,19 @@ const options =
     { value: '#0000ff', label: "Blue" },
     { value: '#55cc77', label: "Teal" },
   ]
-let divStyle = { backgroundColor: options[2].value }
-
+// set initial style elements
+let divStyle = {
+  backgroundColor: options[0].value,
+  width: '30px',
+  height: '30px',
+  position: 'absolute',
+  right: '25px',
+  top: '25px',
+  borderRadius: '15px'
+}
 // handle onChange event of the dropdown
 const handleChange = e => {
   let box = document.getElementById('color_box')
-  let divStyle = { backgroundColor: e.value }
   box.style.backgroundColor = e.value
   console.log(box.style)
 }
