8b187343a6d22fcc7c24b1f4568b079f28830768
Hope4Worthy <brantdeines01@gmail.com>
2022-10-13 14:46:19 -0400

    convert to canvas element class | allow for multiple colors to be drawn on multiple levels | remove colors after 0-5 sec

--- src/Classes/ColorDisplay.tsx
+++ src/Classes/ColorDisplay.tsx
@@ -1,32 +1,49 @@
+export class color_canvas {
+  c: any;
+  ctx: CanvasRenderingContext2D | null;
+  name: string;
 
-export function initCanvas() {
-  var c : any = document.getElementById('my_canvas');
-  var ctx : any = c.getContext('2d');
-  ctx.canvas.width = window.innerWidth;
-  ctx.canvas.height = window.innerHeight - 75;
-}
+  constructor(name: string) {
+    const style =
+      "position: absolute; " +
+      "left: 0px; " +
+      "top: 0px; "
 
-export class circle{
-  ctx: any;
-  color: any;
-  x: number;
-  y: number;
-  size: number;
-  constructor(color: any){
-    var c : any = document.getElementById('my_canvas');
-    this.ctx = c.getContext('2d');
-    this.color = color;
-    this.x = Math.random() * this.ctx.canvas.width;
-    this.y = Math.random() * this.ctx.canvas.height;
-    this.size = Math.random() * (100 - 10) + 10;
-    this.render()
+    this.name = name
+    this.c = document.createElement("canvas");
+    this.ctx = this.c.getContext('2d');
+    this.c.setAttribute('id', name)
+    this.c.setAttribute('style', style)
+    this.c.width = window.innerWidth;
+    this.c.height = window.innerHeight - 95;
+    this.c.style.webkitFilter = "blur(1px)"
+    const ele = document.getElementById('canvas_space')
+    if (ele != null) {
+      ele.append(this.c)
+
+    }
+
+
+
   }
 
-  render() {
-    this.ctx.fillStyle = this.color;
-    this.ctx.beginPath()
-    this.ctx.arc(this.x,this.y,this.size,0,2*Math.PI);
-    this.ctx.closePath()
-    this.ctx.fill();
+  draw(color: string) {
+    if (this.ctx != null) {
+      let x = Math.random() * this.c.width;
+      let y = Math.random() * this.c.height;
+      let size = Math.random() * (100 - 10) + 10;
+      this.ctx.fillStyle = color;
+      this.ctx.beginPath()
+      this.ctx.arc(x, y, size, 0, 2 * Math.PI);
+      this.ctx.closePath()
+      this.ctx.fill();
+    }
+    setTimeout(this.clear.bind(this), Math.random() * 5000) // delete color after 0-5s
+  }
+
+  clear() {
+    if (this.ctx != null) {
+      this.ctx.clearRect(0, 0, this.c.width, this.c.height)
+    }
   }
 }
--- src/windows/MainWindow.tsx
+++ src/windows/MainWindow.tsx
@@ -1,14 +1,14 @@
 import { wait } from "@testing-library/user-event/dist/utils/index.js";
 import Select from "react-select";
-import {initCanvas, circle} from "../Classes/ColorDisplay"
-import {ChangeEvent, FormEvent, useState} from 'react';
+import { color_canvas } from "../Classes/ColorDisplay"
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 const fs = window.require('fs');
 const path = window.require('path');
 
 type Scheme = {
-  name: string,
-  notes: Array<string>
+	name: string,
+	notes: Array<string>
 }
 
 // Reads all color-schemes in 'folderPath/'
@@ -20,8 +20,10 @@ function readSchemesFromFolder(folderPath: string) {
 		if (file.slice(-5) === '.json') {
 			let filePath = path.join(folderPath, file);
 			let data = fs.readFileSync(filePath,
-				{encoding: 'utf8',
-				flag: 'r'});
+				{
+					encoding: 'utf8',
+					flag: 'r'
+				});
 
 			// Format each JSON file into a 2-array of <name> and <notes>
 			schemes.push(JSON.parse(data));
@@ -33,20 +35,32 @@ function readSchemesFromFolder(folderPath: string) {
 
 // set of colors for testing purposes, can be removed later
 const options =
-  [
-    { value: '#000000', label: "Black" },
-    { value: '#808080', label: "Grey" },
-    { value: '#ff0000', label: "Red" },
-    { value: '#00ff00', label: "Green" },
-    { value: '#0000ff', label: "Blue" },
-    { value: '#55cc77', label: "Teal" },
-    { value: '#800080', label: "Purple" }
-  ]
+	[
+		{ value: '#000000', label: "Black" },
+		{ value: '#808080', label: "Grey" },
+		{ value: '#ff0000', label: "Red" },
+		{ value: '#00ff00', label: "Green" },
+		{ value: '#0000ff', label: "Blue" },
+		{ value: '#55cc77', label: "Teal" },
+		{ value: '#800080', label: "Purple" }
+	]
+// create an array of canvas elements to draw one (1 shape per canvas)
+const max_colors = 7
+let color_layers = new Array()
+for (let i = 0; i < max_colors; i++) {
+	color_layers[i] = new color_canvas('c' + i)
+}
+
+// function to draw the shapes of all canvas elements, can be removed later
+function draw_colors() {
+	for (let i = 0; i < max_colors; i++) {
+		color_layers[i].draw(options[i].value)
+	}
+}
+
 export default function MainWindow() {
 	let schemes: Array<Scheme> = [];
 	let ind = 0;
-
-  initCanvas();
 
 	// Get all Default Schemes
 	const pathToDefaultSchemesFolder = path.join('src', 'schemes', 'default');
@@ -56,41 +70,35 @@ export default function MainWindow() {
 	const pathToUserSchemesFolder = path.join('src', 'schemes');
 	schemes = schemes.concat(readSchemesFromFolder(pathToUserSchemesFolder));
 
-  const _12Tones = {Ab:0, A:1, Bb:2, B:3, C:4, Db:5, D:6, Eb:7, E:8, F:9, Gb:10, G:11};
+	const _12Tones = { Ab: 0, A: 1, Bb: 2, B: 3, C: 4, Db: 5, D: 6, Eb: 7, E: 8, F: 9, Gb: 10, G: 11 };
 	let [currScheme, setScheme] = useState(schemes[0]);
 
 	console.log(currScheme);
 
 
-  return (
-    <div>
-      <p>Main Window</p>
-		  <select onChange={(e: ChangeEvent) => setScheme(schemes[parseInt((e.target as HTMLSelectElement).value)])}>
-			  {schemes.map((scheme: Scheme) => <option key={ind} value={ind++}>{scheme.name}</option>)}
-		  </select>
+	return (
+		<div>
+			<p>Main Window</p>
+			<select onChange={(e: ChangeEvent) => setScheme(schemes[parseInt((e.target as HTMLSelectElement).value)])}>
+				{schemes.map((scheme: Scheme) => <option key={ind} value={ind++}>{scheme.name}</option>)}
+			</select>
 
-      {/* Displays the main colors used for the selected color-scheme */}
-      <div style={{display:'flex', flexDirection:'row'}}>
-        <div id='A-color' style={{width:20, height:20, background:currScheme.notes[_12Tones.A]}} />
-        <div id='B-color' style={{width:20, height:20, background:currScheme.notes[_12Tones.B]}} />
-        <div id='C-color' style={{width:20, height:20, background:currScheme.notes[_12Tones.C]}} />
-        <div id='D-color' style={{width:20, height:20, background:currScheme.notes[_12Tones.D]}} />
-        <div id='E-color' style={{width:20, height:20, background:currScheme.notes[_12Tones.E]}} />
-        <div id='F-color' style={{width:20, height:20, background:currScheme.notes[_12Tones.F]}} />
-        <div id='G-color' style={{width:20, height:20, background:currScheme.notes[_12Tones.G]}} />
-      </div>
-
-      <br />
-      <br />
-
-      {/* dropdown for testing the color display, can be removed later */}
-      <div id="color_select">
-        <Select
-          defaultValue={options[0]}
-          options={options}
-          onChange={function(e:any) {new circle(e.value)}}
-        />
-      </div>
-    </div>
-  );
+			{/* Displays the main colors used for the selected color-scheme */}
+			<div style={{ display: 'flex', flexDirection: 'row' }}>
+				<div id='A-color' style={{ width: 20, height: 20, background: currScheme.notes[_12Tones.A] }} />
+				<div id='B-color' style={{ width: 20, height: 20, background: currScheme.notes[_12Tones.B] }} />
+				<div id='C-color' style={{ width: 20, height: 20, background: currScheme.notes[_12Tones.C] }} />
+				<div id='D-color' style={{ width: 20, height: 20, background: currScheme.notes[_12Tones.D] }} />
+				<div id='E-color' style={{ width: 20, height: 20, background: currScheme.notes[_12Tones.E] }} />
+				<div id='F-color' style={{ width: 20, height: 20, background: currScheme.notes[_12Tones.F] }} />
+				<div id='G-color' style={{ width: 20, height: 20, background: currScheme.notes[_12Tones.G] }} />
+			</div>
+			<br />
+			<div id="color_select">
+				<button
+					onClick={function () { draw_colors() }}
+				>Draw Colors</button>
+			</div>
+		</div>
+	);
 }
