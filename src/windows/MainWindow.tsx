be42e90568ce7b0fa947a2b6465af314dd1f8f1b
Hope4Worthy <brantdeines01@gmail.com>
2022-10-21 15:16:41 -0400

    added formula for changing of the intensity of color based on octive

--- src/windows/MainWindow.tsx
+++ src/windows/MainWindow.tsx
@@ -28,9 +28,17 @@ for (let i = 0; i < max_colors; i++) {
 
 // function to draw the shapes of all canvas elements, can be removed later
 function draw_colors(scheme) {
+	/*
 	for (let i = 0; i < max_colors; i++) {
 		color_layers[i].draw_new(scheme.notes[i])
 	}
+	*/
+	color_layers[0].draw_new(scheme.notes[2], -2)
+	color_layers[1].draw_new(scheme.notes[2], -1)
+	color_layers[2].draw_new(scheme.notes[2], 0)
+	color_layers[3].draw_new(scheme.notes[2], 1)
+	color_layers[4].draw_new(scheme.notes[2], 2)
+
 }
 function clear_colors() {
 	for (let i = 0; i < max_colors; i++) {
