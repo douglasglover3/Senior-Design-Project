fdd44c434cb185f3c5b9ee6783b3ea59cbcb58ef
douglasgloverUCF <douglasglover3@knights.ucf.edu>
2023-01-27 20:28:44 -0500

    Added Microphone Settings

--- src/components/MicInput.tsx
+++ src/components/MicInput.tsx
@@ -1,48 +1,72 @@
+import { useState, useRef , useEffect} from 'react';
 export default function MicInput(props){
-    let audioCtx;
-    let microphoneStream;
-    let analyserNode;
-    let audioData;
+    const audioCtx = useRef(null);
+    const interval = useRef(null);
+    const gainNode = useRef(null);
+    const [recording, setRecording] = useState(false);
+    const [volume, setVolume] = useState(0.5)
 
     function startMicInput()
     {
-        audioCtx = new window.AudioContext();
-        microphoneStream = null;
-        analyserNode = audioCtx.createAnalyser()
+        audioCtx.current = new window.AudioContext();
+        gainNode.current = audioCtx.current.createGain();
+        gainNode.current.gain.setValueAtTime(volume, audioCtx.current.currentTime);
+        let microphoneStream = null;
+        let analyserNode = audioCtx.current.createAnalyser()
         analyserNode.fftSize = 8192
         analyserNode.sampleRate = 96000
-        audioData = new Float32Array(analyserNode.fftSize);;
+        let audioData = new Float32Array(analyserNode.fftSize);
         navigator.mediaDevices.getUserMedia ({audio: true})
             .then((stream) =>
             {
-                microphoneStream = audioCtx.createMediaStreamSource(stream);
-                microphoneStream.connect(analyserNode);
+                microphoneStream = audioCtx.current.createMediaStreamSource(stream);
+                microphoneStream.connect(gainNode.current);
+                gainNode.current.connect(analyserNode);
 
                 audioData = new Float32Array(analyserNode.fftSize);
 
-                setInterval(() => {
+                interval.current = setInterval(() => {       
                     analyserNode.getFloatTimeDomainData(audioData);
                     props.transformData(audioData)
                 });
 
             });
+        setRecording(true);
     }
 
-    // function stopMicInput()
-    // {
-    //     MediaStreamTrack.stop();
-    // }
-        
-        return(
-            <div style={{width:"45px"}}>
-                
+    function stopMicInput()
+    {
+        audioCtx.current.close();
+        clearInterval(interval.current);
+        setRecording(false);
+    }
+
+    function changeVolume(inputVolume)
+    {
+        let newVolume = parseFloat(inputVolume.target.value) / 100;
+        if(audioCtx.current != null && audioCtx.current.state == "running")
+            gainNode.current.gain.setValueAtTime(newVolume, audioCtx.current.currentTime);
+        setVolume(newVolume)
+    }
+    
+    return(
+        <div style={{width:"100%"}}>
+            <div style={{width:"80%"}}>
+                {!recording ?
                 <button type="button" onClick={() => startMicInput()}>
                     Start
                 </button>
-                {/* <button type="button" onClick={() => stopMicInput()}>
+                :
+                <button type="button" onClick={() => stopMicInput()}>
                     Stop
-                </button> */}
-                <div id="wave_display"/>
+                </button>
+                }
+                
+                <div>
+                    <input type="range"
+                        value={Math.round(volume * 100)} onChange={changeVolume} />
+                </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
--- src/windows/MainWindow.tsx
+++ src/windows/MainWindow.tsx
@@ -84,7 +84,7 @@ export default function MainWindow() {
 	return (
 		<div>
 			<div id='header'>
-				<h1 style={{textAlign:'center', margin:'20px'}}>Synethize</h1>
+				<h1 style={{textAlign:'center', margin:'20px'}}>Synesthize</h1>
 				<h2 style={{textAlign:'center'}}>A Sound to Color Application</h2>
 			</div>
 			<div id = 'controls' style={{ display: 'flex', flexDirection: 'row', gap:'1rem'}}>
