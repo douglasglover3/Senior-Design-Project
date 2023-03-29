dad2f4a8749ddc617b48ae17351af524bc72eed6
douglasglover3 <douglasglover3@gmail.com>
2023-03-28 21:13:05 -0400

    Added styling and options for intervals

--- src/components/IntervalSelector.tsx
+++ src/components/IntervalSelector.tsx
@@ -43,14 +43,65 @@ export default function IntervalSelector() {
         // Overwrite 'intervals.json' file with new intervals
         IntervalFunctions.saveIntervalsToFile(newIntervals);
     }
+    
+    // Change percentage of interval
+    const handlePercentage = (e): void => {
+        const newPercentage: number = e.target.value;
+        const intervalName: string = e.target.id;
+        let newIntervals: Interval[] = intervals.map((interval) => {
+            // This is the interval we want to modify
+            if (interval.name === intervalName) {
+                return {...interval, percentage: newPercentage};
+            }
+            else {
+                return interval;
+            }
+        });
+        setIntervals(newIntervals);
+
+        // Overwrite 'intervals.json' file with new intervals
+        IntervalFunctions.saveIntervalsToFile(newIntervals);
+    };
+
+    // Change color of interval
+    const handleColor = (e): void => {
+        const newColor: string = e.target.value;
+        const intervalName: string = e.target.id;
+        let newIntervals: Interval[] = intervals.map((interval) => {
+            // This is the interval we want to modify
+            if (interval.name === intervalName) {
+                return {...interval, color: newColor};
+            }
+            else {
+                return interval;
+            }
+        });
+        setIntervals(newIntervals);
+
+        // Overwrite 'intervals.json' file with new intervals
+        IntervalFunctions.saveIntervalsToFile(newIntervals);
+    };
 
     return(
-        <div className='interval-grid'>
-            {intervals.map((interval) => <div className='interval' key={interval.name}>
-                <input value={interval.name} type='checkbox' onChange={handleSelect} checked={interval.isTracked} />
-                <span>{interval.name}</span>
-                <span className='delete-interval' onClick={() => handleDelete(interval.name)}>&times;</span>
-                </div>)}
-        </div>
+        <ul className='interval-grid'>
+            {intervals.map((interval) => 
+                <div className='interval' key={interval.name}>
+                    <div className='interval-container'>
+                        <span className='delete-interval' onClick={() => handleDelete(interval.name)}>&times;</span>
+                        <input value={interval.name} type='checkbox' onChange={handleSelect} checked={interval.isTracked} />
+                        <input type="color" id={interval.name} className='interval-color'
+                            value={interval.color} onChange={handleColor} />
+                        <span>{interval.name}</span>
+                    </div>
+                    <div className='percentage-slider-container'>
+                        <input className='percentage-slider' type="range" id={interval.name}
+                            value={interval.percentage} onChange={handlePercentage} />
+                    </div>
+                </div>
+            )}
+            <button type="button" className='button'>
+                  Add interval
+            </button>
+        </ul>
     );
 }
