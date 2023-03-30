6c1ff3a1a5a8e246562da52d8f6e2718dcf93979
douglasglover3 <douglasglover3@gmail.com>
2023-03-30 03:27:04 -0400

    Implemented adding intervals

--- src/components/IntervalSelector.tsx
+++ src/components/IntervalSelector.tsx
@@ -13,6 +13,10 @@ type Interval = {
 export default function IntervalSelector() {
     // Get list of intervals from 'intervals.json'
     const [intervals, setIntervals]: [Interval[], any] = useState(IntervalFunctions.getIntervals());
+    const [addButtonClicked, setAddButtonClicked]: [boolean, any] = useState(false);
+    const [newIntervalName, setNewIntervalName]: [string, any] = useState("");
+    const [newIntervalLength, setNewIntervalLength]: [number, any] = useState(null);
+    const [errorMessage, setErrorMessage]: [string, any] = useState("");
     useEffect(() => {
         IntervalFunctions.setIntervals(intervals);
     }, [intervals]);
@@ -26,6 +30,37 @@ export default function IntervalSelector() {
 
         // Overwrite 'intervals.json' file with new intervals
         IntervalFunctions.saveIntervalsToFile(newIntervals);
+    }
+
+    // Remove this interval from the list entirely
+    const handleAdd = (): void => {
+        let search: Interval[] = intervals.filter(interval => interval.name == newIntervalName); 
+        if(newIntervalName.length == 0)
+            setErrorMessage("Needs Interval Name.")
+        else if(newIntervalLength == null || newIntervalLength == 0)
+            setErrorMessage("Needs Interval Length.")
+        else if(search.length != 0)
+            setErrorMessage("Interval Name is taken.")
+        else
+        {
+            intervals.push(
+                {
+                    name: newIntervalName,
+                    intervalLength: newIntervalLength,
+                    color: "#FF0000",
+                    percentage: 0
+                }
+            )
+            resetUI();
+            // Overwrite 'intervals.json' file with new intervals
+            IntervalFunctions.saveIntervalsToFile(intervals);
+        }
+    }
+    const resetUI = (): void => {
+        setAddButtonClicked(false);
+        setErrorMessage("");
+        setNewIntervalName("")
+        setNewIntervalLength(null);
     }
     
     // Change percentage of interval
@@ -68,23 +103,45 @@ export default function IntervalSelector() {
 
     return(
         <ul className='interval-grid'>
-            {intervals.map((interval) => 
-                <div className='interval' key={interval.name}>
-                    <div className='interval-container'>
-                        <span className='delete-interval' onClick={() => handleDelete(interval.name)}>&times;</span>
-                        <input type="color" id={interval.name} className='interval-color'
-                            value={interval.color} onChange={handleColor} />
-                        <span>{interval.name}</span>
-                    </div>
-                    <div className='percentage-slider-container'>
-                        <input className='percentage-slider' type="range" id={interval.name}
-                            value={interval.percentage} onChange={handlePercentage} />
-                    </div>
+            {!addButtonClicked ? 
+                <div>
+                    {intervals.map((interval) => 
+                        <div className='interval' key={interval.name}>
+                            <div className='interval-container'>
+                                <span className='delete-interval' onClick={() => handleDelete(interval.name)}>&times;</span>
+                                <input type="color" id={interval.name} className='interval-color'
+                                    value={interval.color} onChange={handleColor} />
+                                <span>{interval.name}</span>
+                            </div>
+                            <div className='percentage-slider-container'>
+                                <input className='percentage-slider' type="range" id={interval.name}
+                                    value={interval.percentage} onChange={handlePercentage} />
+                            </div>
+                        </div>
+                    )}
+                    <button type="button" className='button' onClick={() => setAddButtonClicked(true)}>
+                        Add interval
+                    </button>
                 </div>
-            )}
-            <button type="button" className='button'>
-                  Add interval
-            </button>
+            :
+            <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <input style={{width: "80%", marginBottom: "2%"}} placeholder="Interval Name" type="text" className='input-field'
+                    required autoFocus
+                    onChange = {(e) => setNewIntervalName(e.target.value.trim())} />
+                <input style={{width: "80%", marginBottom: "2%"}} placeholder="Interval Length" type="number" className='input-field'
+                    required
+                    onChange = {(e) => setNewIntervalLength(e.target.value)} />
+                <div style={{display: "flex", flexDirection: "row", justifyContent: "center", width: "80%"}}>
+                    <button type="button" className='button' onClick={() => resetUI()}>
+                        Back
+                    </button>
+                    <button type="button" className='button' onClick={() => handleAdd()}>
+                        Confirm
+                    </button>
+                </div>
+                <p style={{color: "red", margin: "0px"}}>{errorMessage}</p>
+            </div>
+            }
         </ul>
     );
 }
